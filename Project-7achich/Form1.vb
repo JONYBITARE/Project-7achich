ca50bc7d212058203552c2c824d96c3eed7cd1c8
jonybitar9@gmail.com <jonybitar9@gmail.com>
2021-01-11 20:55:23 +0200

    123

--- Project-7achich/Form1.vb
+++ Project-7achich/Form1.vb
@@ -1,17 +1,5 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("hello")
-        MsgBox("hello1123123")
-        MsgBox("hello12")
-        MsgBox("hello112221")
-        MsgBox("jony")
-        MsgBox("jony11")
-        MsgBox("jony11123")
-        MsgBox("1231231312312")
-        MsgBox("hello new")
-        MsgBox("hello 112")
-        MsgBox("hello 22")
-        MsgBox("hello 22123")
-        MsgBox("hellonew one")
+
     End Sub
 End Class
