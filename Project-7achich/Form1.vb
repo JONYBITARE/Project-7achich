4eb2d2c7df92e244e22025c000e6aab43bf7991f
jonybitar9@gmail.com <jonybitar9@gmail.com>
2021-01-11 23:32:23 +0200

    nfe5o

--- Project-7achich/Form1.vb
+++ Project-7achich/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MsgBox("msg1")
-        MsgBox("msg33")
+        MsgBox("msg3345")
+        MsgBox("msg312")
+        MsgBox("msg33123")
+        MsgBox("msg3144")
     End Sub
 End Class
