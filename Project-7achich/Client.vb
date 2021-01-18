ce28fd0917d9e589acfdc77f3b9812772c983432
Alex <HP@WIN8>
2021-01-18 17:35:11 +0200

    changes

--- Project-7achich/Client.vb
+++ Project-7achich/Client.vb
@@ -4,16 +4,15 @@ Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.XtraGrid.Columns
 Imports System.Data.SqlClient
 Imports DevExpress.XtraBars.Docking2010
-Partial Class Client
+Public Class Client
     Dim ds As New DataSet
     Dim da As New SqlDataAdapter
-    Dim con As New SqlConnection("")
-
+    Dim con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\source\repos\Project-7achich\Project-7achich\database1.mdf;Integrated Security=True;Connect Timeout=30")
     Private Sub Client_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub btngrp_CustomButtonClick(sender As Object, e As BaseButtonEventArgs) Handles btngrp.CustomButtonClick
+    Private Sub Btngrp_CustomButtonClick(sender As Object, e As BaseButtonEventArgs) Handles btngrp.CustomButtonClick
         If e.Button.Properties.ImageIndex = 0 Then
             Add_Client.Show()
         ElseIf e.Button.Properties.ImageIndex = 1 Then
